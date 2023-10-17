bed5232a4eea742c53d947e1d1d96865be3a2529
7mantel7 <aper29012006@gmail.com>
2023-10-17 21:23:35 +0300

    f

--- project/Character.h
+++ project/Character.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Entity.h"
+#include <string>
+
+class Character : public Entity {
+protected:
+	int hp;
+	int maxhp;
+	int strenght;
+	int agility;
+	int level;
+	int damage;
+public:
+	Character(std::string name, int hp, int maxhp, int strenght, int agility, int level, int damage);
+};
